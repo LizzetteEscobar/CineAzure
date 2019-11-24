e7ebe9516588b8ff34c89083cf34b29a86df553a
mi pc <wendy_lizzette@rocketmail.com>
2019-11-24 12:04:34 -0600

    editar

--- app/src/main/java/com/example/cineazure/Fragments/ClaquetaFragment.java
+++ app/src/main/java/com/example/cineazure/Fragments/ClaquetaFragment.java
@@ -6,23 +6,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.example.cineazure.*;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ClaquetaFragment extends Fragment {
+public class ClaquetaFragment extends Fragment implements View.OnClickListener{
 
-
-    public ClaquetaFragment() {
-        // Required empty public constructor
-    }
-
-
+    Button btnestreno;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_claqueta, container, false);
+        View v=inflater.inflate(R.layout.fragment_claqueta, container, false);
+
+        return v;
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnestreno:
+                Intent intent = new Intent(getContext(), DeltallePelicula.class);
+                ((MainActivity) getActivity()).startActivity(intent);
+                break;
+        }
     }
 }
